8e23b80d6dc7011b2ab81815b0bfd095e72d6d30
Hoon Kim <hoonsubin@icloud.com>
2020-10-30 02:09:47 -0700

    added template args

---
+++ scripts/run.ts
@@ -0,0 +1,50 @@
+//note: this cli script is for testing and being executed directly from source
+
+import path from 'path';
+import { GlitchGit, GlitchProject } from '../src/models';
+
+const gitPushTest = async () => {
+    const source = process.env.REPO_SOURCE;
+
+    if (typeof source === 'undefined') {
+        throw new Error('target repository has not been provided');
+    }
+
+    // the folder to where it will copy things
+    const targetFolder = 'target';
+
+    const glitchRepo = new GlitchGit(source, true);
+
+    await glitchRepo.publishFilesToGlitch(path.join(__dirname, targetFolder));
+};
+
+const gitImportTest = async () => {
+    const projId = process.env.GLITCH_PROJ_ID;
+    const token = process.env.GLITCH_USER_TOKEN;
+
+    if (typeof projId === 'undefined' || typeof token === 'undefined') {
+        throw new Error('the required environment variables were not provided');
+    }
+
+    const sourceRepo = 'staketechnologies/lockdrop-ui';
+    const glitchProj = new GlitchProject(token, projId);
+
+    const res = await glitchProj.importFromGithub(sourceRepo, 'public');
+
+    if (res.status !== 200) {
+        throw new Error('Failed to import project');
+    }
+    console.log('successfully imported project ' + sourceRepo);
+};
+
+// script entry point
+(async () => {
+    throw new Error('Script not implemented!');
+    //await gitPushTest();
+    //await gitImportTest();
+
+    //process.exit(0);
+})().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,13 +1,60 @@
-// everything in the cli folder should be isolated from the reset of the code.
-// the src/index.ts is meant to export the project classes that are used in this tool for developers.
-// everything in the cli folder is meant for functions directly executed by the user via node.
+#!/usr/bin/env ts-node
+
+/**
+ * everything in the cli folder should be isolated from the reset of the code.
+ * the src/index.ts is meant to export the project classes that are used in this tool for developers.
+ * everything in the cli folder is meant for functions directly executed by the user via node.
+ */
+import dashdash, { ParserConfiguration } from 'dashdash';
+
+interface ScriptArgument {
+    name: string;
+    options: ParserConfiguration['options'];
+}
+
+const scriptArgs = [{ name: 'push-files', type: 'string' }];
+
+const options: ParserConfiguration['options'] = [
+    {
+        name: 'version',
+        type: 'bool',
+        help: 'Print tool version and exit.',
+    },
+    {
+        names: ['help', 'h'],
+        type: 'bool',
+        help: 'Print this help and exit.',
+    },
+    {
+        names: ['verbose', 'v'],
+        type: 'arrayOfBool',
+        help: 'Verbose output. Use multiple times for more verbose.',
+    },
+    {
+        names: ['repo', 'r'],
+        type: 'string',
+        help: 'Repository to clone',
+        env: 'REPO_SOURCE',
+    },
+    {
+        names: ['file', 'f'],
+        type: 'string',
+        help: 'File to process',
+        helpArg: 'FILE',
+    },
+];
 
 // CLI tool entry point
 (async () => {
-    console.log('Hello world');
+    const parser = dashdash.createParser({ options: options });
+    const opts = parser.parse(process.argv);
+    const args = opts._args;
+
+    console.log(args);
+    console.log(opts._order);
 
     process.exit(0);
 })().catch((err) => {
-    console.error(err);
+    console.error('error: ' + err.message);
     process.exit(1);
 });
