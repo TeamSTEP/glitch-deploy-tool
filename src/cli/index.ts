9d237d0bbd31f115c31e1f45609ecb73c49e3194
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2020-11-21 20:29:42 +0900

    separated command code (#13)

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,102 +5,9 @@
  * the src/index.ts is meant to export the project classes that are used in this tool for developers.
  * everything in the cli folder is meant for functions directly executed by the user via node.
  */
-import dashdash, { ParserConfiguration } from 'dashdash';
+import dashdash from 'dashdash';
 import * as Deployer from './deploy';
-
-const defaultOptions: ParserConfiguration['options'] = [
-    {
-        group: 'Tool Information',
-    },
-    {
-        name: 'version',
-        type: 'bool',
-        help: 'Print tool version and exit.',
-    },
-    {
-        names: ['help', 'h'],
-        type: 'bool',
-        help: 'Print this help and exit.',
-    },
-];
-
-const commonOpts: ParserConfiguration['options'] = [
-    {
-        names: ['path', 'p'],
-        type: 'string',
-        help: 'Folder to upload to Glitch',
-        completionType: 'file',
-        helpArg: 'PATH',
-    },
-
-    ...defaultOptions,
-];
-
-const uploadLocalOpts: ParserConfiguration['options'] = [
-    {
-        group: 'Upload Local Folder Options',
-    },
-    {
-        names: ['remote', 'r'],
-        type: 'string',
-        env: 'REPO_SOURCE',
-        help: "Glitch project repository's remote URL",
-        helpArg: '<glitch git url>',
-    },
-    {
-        names: ['verbose', 'v'],
-        type: 'bool',
-        help: 'Verbose output. This will log all debug messages.',
-    },
-    ...commonOpts,
-];
-
-const importGhOpts: ParserConfiguration['options'] = [
-    {
-        group: 'Import from Github Options',
-    },
-    {
-        names: ['token', 't'],
-        type: 'string',
-        env: 'GLITCH_USER_TOKEN',
-        help: 'Glitch user API token used to access the account.',
-        helpArg: '<glitch user token>',
-    },
-    {
-        names: ['glitch-id', 'i'],
-        type: 'string',
-        env: 'GLITCH_PROJ_ID',
-        help: 'Glitch project ID that will',
-        helpArg: '<glitch user token>',
-    },
-    {
-        names: ['repo', 'r'],
-        type: 'string',
-        env: 'REPO_SOURCE',
-        help: 'Github repository name that will be imported to Glitch.',
-        helpArg: '<user name/project name>',
-    },
-    ...commonOpts,
-];
-
-interface SubCommand {
-    subcommand: string;
-    options: ParserConfiguration['options'];
-    help: string;
-}
-
-const scriptArgs: SubCommand[] = [
-    {
-        subcommand: 'github',
-        options: importGhOpts,
-        help: '',
-    },
-    {
-        subcommand: 'from-local',
-        options: uploadLocalOpts,
-        help: '',
-    },
-];
+import scriptArgs, { defaultOptions } from './command';
 
 const checkToolMode = () => {
     const userArg = process.argv.slice(2)[0];
@@ -128,13 +35,6 @@ const checkToolMode = () => {
         const currentCommand = mode.subcommand || '[from-local | github]';
         //const commandList =
         console.log(`usage: glitch-deploy-tool ${currentCommand} [OPTIONS]\noptions:\n${help}\n`);
-        process.exit(0);
-    }
-
-    // print the tool version
-    if (opts.version) {
-        //todo: dynamically parse the app version without importing package.json (due to tsc folder structure)
-        console.log(`glitch-deploy-tool version 0.1.4-alpha`);
         process.exit(0);
     }
 
